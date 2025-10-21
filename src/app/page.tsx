8d7dd101fa8590ef51e73bfa044c662d16d67fd6
Manas0410 <manasshrivastava0410@gmail.com>
2025-10-21 23:11:26 +0530

    nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Banner } from "@/Sections/banner";
 import { AppImages } from "@/Sections/app-images";
 import Image from "next/image";
+import { NavBar } from "@/Sections/nav-bar";
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <Banner />
       <AppImages />
     </>
