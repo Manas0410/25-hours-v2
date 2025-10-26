30fb0dc8b67b25741c5aa298f753a2839c25c686
Sanskrati Agrawal <55913586+sanskrati-1234@users.noreply.github.com>
2025-10-26 19:17:06 +0530

    Update aboutAIfeatures.tsx

--- src/Sections/aboutAIfeatures.tsx
+++ src/Sections/aboutAIfeatures.tsx
@@ -17,7 +17,7 @@ export function AboutAIFeat() {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+        <div className="text-3xl md:text-5xl font-bold dark:text-white text-center">
           Get your tasks planned by AI
         </div>
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
