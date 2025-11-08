6c516013436b2e96ee9dba10d3fbe860cd79bf08
Sanskrati Agrawal <55913586+sanskrati-1234@users.noreply.github.com>
2025-11-08 18:41:49 +0530

    Update navbar.tsx

--- src/Sections/navbar.tsx
+++ src/Sections/navbar.tsx
@@ -57,7 +57,7 @@ export function MainNavbar() {
               as="button"
               onClick={scrollToContact}
             >
-              Demo
+              Schedule demo
             </NavbarButton>
           </div>
         </NavBody>
